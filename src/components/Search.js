43d2d956373c0f45c4807737f69fe9f7f2aeaadf
oscar <oscarcastellote1@gmail.com>
2021-09-03 16:28:10 +0200

    ogcs added pagination (it still needs to be adjusted)

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react"
+import { act } from "react-dom/cjs/react-dom-test-utils.production.min"
 // import Pagination from "react-js-pagination"
-// require("bootstrap/less/bootstrap.less")
+import ReactPaginate from "react-paginate"
 
 import Card from "../UI/Card"
 import "./Search.css"
@@ -10,8 +11,10 @@ const Search = React.memo((props) => {
   const [enteredFilter, setEnteredFilter] = useState("")
   const [gifArray, setGifArray] = useState([{ id: 0, title: "", url: "" }])
   const [activePage, setActivePage] = useState(1)
+  const [totalCount, setTotalCount] = useState(0)
   const [foundGifs, setFoundGifs] = useState(false)
   const inputRef = useRef()
+  const limit = 5
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -19,7 +22,11 @@ const Search = React.memo((props) => {
         enteredFilter === inputRef.current.value &&
         enteredFilter.length !== 0
       ) {
-        const query = `?q="${enteredFilter}"&api_key=dc6zaTOxFJmzC&limit=5`
+        setActivePage(1) /////
+        let offset = activePage * limit - (limit - 1) //offset=0 or undefined causes response.ok==false, then it has to start at 1.
+        const query = `?q="${enteredFilter}"&api_key=dc6zaTOxFJmzC&limit=${limit}&offset=${offset}`
+
+        console.log("query", query)
         fetch("http://api.giphy.com/v1/gifs/search" + query) // Example'http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5&offset=4'
           .then((response) => {
             console.log("response.ok", response.ok)
@@ -41,12 +48,13 @@ const Search = React.memo((props) => {
                   title: response.data[key].images.original.title,
                   url: response.data[key].images.original.url,
                 })
-                // setTotalCount(response.pagination.total_count)
+                setTotalCount(response.pagination.total_count)
               }
               setGifArray(gifAuxArray)
             } else {
               setGifArray([{ id: 0, title: "", url: "" }])
-              // setTotalCount(0)
+              setTotalCount(0)
+              setActivePage(1) ////
               //no Gifs found
             }
           })
@@ -55,12 +63,12 @@ const Search = React.memo((props) => {
     return () => {
       clearTimeout(timer)
     }
-  }, [enteredFilter, inputRef])
+  }, [enteredFilter, inputRef, activePage])
 
-  // handlePageChange(pageNumber) {
-  //   console.log(`active page is ${pageNumber}`);
-  //   setActivePage(pageNumber);
-  // }
+  const handlePageChange = (pageNumber) => {
+    console.log(`active page is ${pageNumber.selected}`)
+    setActivePage(pageNumber.selected)
+  }
 
   return (
     <section className="search">
@@ -76,16 +84,24 @@ const Search = React.memo((props) => {
             value={enteredFilter}
             onChange={(event) => setEnteredFilter(event.target.value)}
           />
-          {/* <Pagination
-          activePage={activePage}
-          itemsCountPerPage={10}
-          totalItemsCount={450}
-          pageRangeDisplayed={5}
-          onChange={handlePageChange}
-          /> */}
           {/* {totalCount !== 0 && <h4>5 results out of {totalCount}</h4>} */}
         </div>
       </Card>
+      <div>
+        <ReactPaginate
+          marginPagesDisplayed={2}
+          pageCount={totalCount}
+          pageRangeDisplayed={limit}
+          onPageChange={handlePageChange}
+          containerClassName={"container"}
+          previousLinkClassName={"page"}
+          breakClassName={"page"}
+          nextLinkClassName={"page"}
+          pageClassName={"page"}
+          disabledClassNae={"disabled"}
+          activeClassName={"active"}
+        />
+      </div>
       <div>
         {foundGifs ? (
           gifArray.map((item) => {
