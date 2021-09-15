35111e57758a5456289f1d5888a5c09e7ba3d516
oscar <oscarcastellote1@gmail.com>
2021-09-15 12:16:17 +0200

    ogcs cleanup

--- src/hooks/useHandleFetch.tsx
+++ src/hooks/useHandleFetch.tsx
@@ -31,11 +31,9 @@ const useHandleFetch = (): FetchResult => {
       offset = 4999 //08/09/2021 the query does not accept offset>4999
     }
     const query: string = `?q="${enteredFilter}"&api_key=${apiKey}&limit=${limit}&offset=${offset}`
-    console.log("query", query)
     setIsLoading(true)
     fetch("http://api.giphy.com/v1/gifs/search" + query)
       .then((response) => {
-        console.log("response.ok", response.ok)
         if (response.ok) {
           setFoundGifs(true)
           return response.json()
@@ -48,7 +46,6 @@ const useHandleFetch = (): FetchResult => {
         }
       })
       .then((response) => {
-        console.log("response", response)
         setIsLoading(false)
         if (typeof response !== "undefined") {
           const gifAuxArray: Data[] = []
